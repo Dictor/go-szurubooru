c8d30a5d81763603cfa2f708a67417b2b264fa92
Dictor <kimdictor@gmail.com>
2020-03-31 13:54:55 +0000

    Enhance models. * Add user model * Assign reciever in all method for cleary classfying methods.

--- server/model/tag_category.go
+++ server/model/tag_category.go
@@ -17,39 +17,28 @@ func (TagCategory) TableName() string {
 	return "table_category"
 }
 
-func NewTC(name, color string) *TagCategory {
-	return &TagCategory{0, name, color, 0, false, gorm.Model{}}
-}
-
-func GetAllTC() ([]*TagCategory, error) {
+func (TagCategory) GetAll() ([]*TagCategory, error) {
 	res := make([]*TagCategory, 0)
 	return res, db.Find(&res).Error
 }
 
-func GetTC(name string) (*TagCategory, error) {
-	res := TagCategory{}
-	return &res, db.Where("name = ?", name).First(&res).Error
+func (tc *TagCategory) Get() (*TagCategory, error) {
+	return &res, db.Where("name = ?", tc.Name).First(tc).Error
 }
 
-func CreateTC(tc *TagCategory) error {
+func (tc *TagCategory) Create() error {
 	return db.Create(tc).Error
 }
 
-func UpdateTC(name string, color string, version int) error {
-	if tc, err := GetTC(name); err != nil {
-		return err
-	} else {
-		tc.Color = color
-		tc.Version = version
-		return db.Save(&tc).Error
-	}
+func (tc *TagCategory) Update() error {
+	return db.Model(tc).Updates(tc).Error
 }
 
-func DeleteTC(name string) error {
-	return db.Where("name = ?", name).Delete(&TagCategory{}).Error
+func (tc *TagCategory) Delete() error {
+	return db.Where("name = ?", tc.Name).Delete(tc).Error
 }
 
-func SetDefaultTC(name string) (*TagCategory, error) {
+func (TagCategory) SetDefault(name string) (*TagCategory, error) {
 	if tc, err := GetTC(name); err != nil {
 		return nil, err
 	} else {
